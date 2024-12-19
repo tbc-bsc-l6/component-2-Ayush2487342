32086e09b6873af038a4b919b14180005c0b557c
Ayush2487342 <kayush22@tbc.edu.np>
2024-12-19 22:32:45 +0545

    brainstroming
    little modifications

--- component2/app/Http/Controllers/Controller.php
+++ component2/app/Http/Controllers/Controller.php
@@ -1,8 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
-abstract class Controller
+use App\MOdels\Post;
+use illumiante\Http\Controllers;
+class Controller extends Controller
 {
-    //
+    // brianstroming now 
+    public function index()
+    {
+        $post= Post ::all();
+        return response()->json($posts);
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function
+    {
+        $
+    }
 }
+
+
