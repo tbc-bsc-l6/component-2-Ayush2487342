6a42efd138c752dec73d94c671202f5a87ee270c
Ayush2487342 <kayush22@tbc.edu.np>
2025-01-01 19:34:58 +0545

    further working with controllers

---
+++ component2/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title', 'Home')
+@section('content')
+    <h1>Welcome to Phone Shop</h1>
+@endsection
--- component2/routes/web.php
+++ component2/routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Homepage Route
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Product Routes
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+
+// Cart Routes
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
+Route::delete('/cart/{id}', [CartController::class, 'destroy'])->name('cart.destroy');
+
+// Checkout Routes
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
+Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
+
